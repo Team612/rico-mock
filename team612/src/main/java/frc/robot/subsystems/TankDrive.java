fa6f086d791225b2350901d472ab7b0c59a68922
Keel <1779704@fcps.edu>
2019-11-07 20:23:05 -0500

    Fixing branch

--- team612/src/main/java/frc/robot/subsystems/TankDrive.java
+++ team612/src/main/java/frc/robot/subsystems/TankDrive.java
@@ -7,18 +7,28 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.DefaultTank;
 
 /**
  * Add your docs here.
  */
 public class TankDrive extends Subsystem {
+  System.out.println("Hi");
+  public WPI_TalonSRX talon_FL = new WPI_TalonSRX(RobotMap.TANK_PORT_FL);
+  public WPI_TalonSRX talon_FR = new WPI_TalonSRX(RobotMap.TANK_PORT_FR);
+  public WPI_TalonSRX talon_BL = new WPI_TalonSRX(RobotMap.TANK_PORT_BL);
+  public WPI_TalonSRX talon_BR = new WPI_TalonSRX(RobotMap.TANK_PORT_BR);
+ 
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+     setDefaultCommand(new DefaultTank());
   }
 }
