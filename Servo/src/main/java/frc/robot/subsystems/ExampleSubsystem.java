9d75101487b60c4233cc96cbe954d3b72aefc60b
Myrrh Khan <myrrhkhan@gmail.com>
2019-11-14 20:42:08 -0500

    Code for servo

---
+++ Servo/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.ExampleCommand;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class ExampleSubsystem extends Subsystem {
+  //Follow template for objects.
+  public Servo servo1 = new Servo(0);
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    // This allows for the subsystem to be linked to the command (robot/commands/ExampleCommand)
+    setDefaultCommand(new ExampleCommand());
+  }
+}
